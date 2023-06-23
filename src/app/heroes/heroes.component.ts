7a88546129905ce029f2e47b3d87a7277cfc443c
Christian Cabrejas <christiancabrejas@gmail.com>
2023-06-23 13:47:55 +0200

    feat(hero-navigation): Adding the navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,39 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  
-  heroes: Hero[] = []; 
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  selectedHero?: Hero;
+  constructor(private heroService: HeroService) { }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
-  }
-
-  constructor(private heroService: HeroService
-            , private messageService: MessageService){}
-
-  getHeroes(): void{
-    this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.getHeroes();
   }
 
-  test(){
-    console.log("test");
-    
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
-
 }
